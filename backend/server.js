a8e9a0fef5bc50b757c0bef7008501fd2e1fda81
zkomunika <mzakyalmubarok9.4@gmail.com>
2026-03-13 13:44:37 +0700

    fix CORS

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(helmet());
 app.use(cors({
   origin: ['http://localhost:5500', 'http://127.0.0.1:5500', 'https://dompetku-uts.netlify.app', '*'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'ngrok-skip-browser-warning']
 }));
 
 // Bypass Ngrok browser warning
